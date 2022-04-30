e487bf51b0c45beee96a6cbaa461d8b57f543c4e
Marien Mupenda <marien.mupenda@gmail.com>
2022-04-30 11:55:19 +0200

    update author on error message

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Marien Mupeda">
+    <meta name="author" content="Marien Mupenda">
 
     <title>@yield('code') @yield('title')</title>
 
