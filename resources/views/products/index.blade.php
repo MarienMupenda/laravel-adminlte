002b32df63e9ebe194d71702e5b6be508da824a0
Marien Mupenda <marien.mupenda@gmail.com>
2022-04-30 20:04:08 +0200

    create product views

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,43 @@
+@extends('adminlte::page')
+@section('content_header')
+    <h1>{{$title}}</h1>
+@stop
+@section('content')
+    <div class="p-0 container-fluid ">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-7">
+                    <div class="card-header">
+                        <div class="m-0 box_header">
+                            <div class="main-title">
+                                <h3 hidden class="m-0">{{__($title)}}</h3>
+                            </div>
+                            <div class="erning_btn d-flex">
+                                <a href="{{route('products.create')}}" class="btn btn-outline-primary btn-sm"><i
+                                        class="fa fa-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3 card-body">
+                    <!-- <items company="{{auth()->user()->company_id}}"></items>-->
+                        <!-- table-responsive -->
+                        <div class="table-responsive m-b-40">
+                            {{-- With buttons --}}
+                            <x-adminlte-datatable id="table7" :heads="$heads" theme="light" :config="$config" striped
+                                                  hoverable with-buttons/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('css')
+    <style>
+        .uz-img {
+            width: auto;
+            height: 25px;
+        }
+
+    </style>
+@stop
