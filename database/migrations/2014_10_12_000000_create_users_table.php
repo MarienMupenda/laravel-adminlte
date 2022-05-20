85a66df3ad09b4dfed77e4560dd654753b28e688
Marien Mupenda <marien.mupenda@gmail.com>
2022-05-20 13:29:03 +0200

    add migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->enum('role', ['admin', 'user'])->default('user');
             $table->string('password');
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Activity;
+use App\Models\CabCategory;
+use App\Models\Device;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        $this->call([
+            UserSeeder::class,
+        ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+}
